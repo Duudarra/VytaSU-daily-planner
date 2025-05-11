ff2d27577cbf9e51dae4abdb9b66f71fbae3a620
Duudarra <117339884+Duudarra@users.noreply.github.com>
2025-05-11 14:45:41 +0300

    Update pars.py

--- pars.py
+++ pars.py
@@ -1,13 +1,17 @@
 import asyncio
-
 from models import async_main
 from parsing import start_parsing
+import logging
 
+# Настройка логирования в stdout
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 async def main():
-    await async_main()  # Initialize the database
-    await start_parsing()  # Start the parsing loop
-
+    logger.info("Запуск парсера")
+    await async_main()  # Инициализация базы данных
+    await start_parsing()  # Запуск парсинга
+    logger.info("Парсинг завершен")
 
 if __name__ == "__main__":
     asyncio.run(main())
