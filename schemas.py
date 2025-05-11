28542ee73a1f17213018a4b2f88c1f9fc2776a4b
Darya <kokov.dara.8104@gmail.com>
2025-05-11 22:44:53 +0300

    fix token

--- schemas.py
+++ schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 from datetime import date
 from typing import Optional
 
@@ -26,7 +26,7 @@ class UserOut(BaseModel):
     is_active: bool
 
     class Config:
-        orm_mode = True
+        model_config = ConfigDict(from_attributes=True)
 
 class Token(BaseModel):
     access_token: str
